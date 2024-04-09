fc0e5ae4b89b9d7435eab0918e1a4bb73f0181f2
ilhamarkan2004 <ilhamarkan2004@gmail.com>
2024-04-09 20:08:42 +0700

    admin dsh

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -29,34 +29,14 @@ const Home = () => {
                         <img
                             src="build/assets/images/hero_image.png"
                             alt="Product"
-                            className="w-32 h-28 rounded-md lg:rounded-xl lg:w-48 lg:h-44  "
+                            className="w-32 h-28 rounded-md lg:rounded-xl lg:w-48 lg:h-44"
                         />
-                        <div className="ml-4  w-full py-3 ">
-                            <h3 className="text-lg lg:text-2xl text-black font-semibold leading-none">
+                        <div className="ml-4 w-full flex flex-col justify-between lg:py-3 text-justify">
+                            <h3 className="text-lg lg:text-2xl xl:text-3xl text-black font-semibold">
                                 Lorem ipsum dolor sit amet consectetur.
                             </h3>
-                            <p className="text-slate-900 text-sm lg:text-base ">
-                                Lorem ipsum dolor sit amet consectetur
-                                adipisicing elit. Deleniti in atque provident
-                                nostrum alias dolorem.
-                            </p>
-                        </div>
-                    </div>
-                    <div className="flex mb-5">
-                        <img
-                            src="build/assets/images/hero_image.png"
-                            alt="Product"
-                            className="w-32 h-28 rounded-md lg:rounded-xl lg:w-48 lg:h-44 "
-                        />
-                        <div className="ml-4  w-full py-3 ">
-                            <h3 className="text-lg lg:text-2xl text-black font-semibold leading-none">
-                                Lorem ipsum dolor sit amet consectetur.
-                            </h3>
-                            <p className="text-slate-900 text-sm lg:text-base ">
-                                Lorem ipsum dolor sit amet consectetur
-                                adipisicing elit. Deleniti in atque provident
-                                nostrum alias dolorem.
-                            </p>
+                            <p className="text-slate-900 text-sm lg:text-base text-ellipsis overflow-hidden line-clamp-3 lg:line-clamp-4 mt-auto">
+                              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus optio aut, soluta, error debitis vel ipsum rem aspernatur repudiandae unde ex accusamus itaque nobis exercitationem totam, saepe alias dolorum vero eos nostrum odit? Nisi id aperiam quis laboriosam magnam odit tempore impedit, vitae doloremque. Nemo ipsa quasi corporis earum numquam, eos quidem provident eius distinctio? Debitis quis doloremque quo consequuntur beatae quos, voluptatibus non? Neque iusto doloribus quo dicta quia consectetur eum veritatis cupiditate blanditiis obcaecati nam esse, fugit dolore similique alias minima error facere delectus voluptas dolorem quaerat assumenda sapiente, placeat in. Quod commodi illum modi cum impedit, dolorem ullam minus enim excepturi doloribus porro ipsa quas et dolores quos doloremque totam delectus quae! Sunt aspernatur necessitatibus alias quasi molestiae tenetur debitis ab, odio laborum inventore id recusandae, autem dicta nihil, numquam eum ut saepe sequi ad. Modi quos perspiciatis nobis? Saepe culpa laudantium sunt iste a optio aspernatur, iure voluptates magni architecto, quaerat quo eos ducimus nobis! Repudiandae, illum. Fuga earum, aliquam quia fugiat cumque praesentium repellat ipsum expedita. Ducimus cumque quisquam esse sed nostrum, odit iure sequi reprehenderit culpa praesentium voluptatibus quibusdam qui labore, repudiandae eligendi odio dolor accusantium quidem asperiores quam. Quaerat quisquam eos animi natus quo voluptas qui deleniti laudantium blanditiis. Dolor nesciunt corporis quibusdam sit nostrum. Labore repellendus soluta harum optio laborum amet maiores dignissimos alias laudantium eum tenetur eaque ducimus obcaecati a repudiandae corrupti quod quidem quam, officia dolores perferendis! Molestias eligendi sint aperiam fugiat animi. Inventore, saepe dignissimos! Quaerat molestias, laudantium architecto delectus ducimus at mollitia porro repellendus molestiae, nobis doloremque voluptas culpa sint repudiandae velit perferendis voluptatem. Nihil corrupti hic expedita illum perferendis, culpa suscipit fuga saepe odit laboriosam ipsum error nisi, ducimus officia vitae! Harum numquam alias suscipit repudiandae nobis aut enim totam at voluptatum quas veniam, saepe voluptatem nulla consequatur atque laborum necessitatibus. Mollitia nisi enim minima illum nesciunt ullam adipisci voluptates magnam consequuntur atque ducimus animi autem maxime, aspernatur sint libero vero et ab omnis, aut expedita culpa, reprehenderit molestiae velit. Earum perferendis error quae odio maxime rem, pariatur maiores excepturi, assumenda alias culpa voluptatem vitae atque consequatur explicabo dolores dolore ullam laudantium ad blanditiis possimus! Modi recusandae facilis harum magni iusto voluptates ut mollitia inventore ratione, error eaque, ad sequi, ducimus dignissimos. Totam excepturi sequi placeat labore ipsa voluptates illo incidunt. Illum nulla quo sequi harum rem ducimus sint veritatis, dolorum doloremque minima accusantium ex? Quibusdam, explicabo laudantium! Aperiam iste rerum facere sequi fugiat, quisquam et, cum facilis porro itaque quae ratione tenetur alias minus corrupti! Veritatis quia molestias corrupti facere ratione, suscipit perferendis enim est dolor nemo repellat cumque voluptates explicabo dolore nesciunt ut? Velit earum soluta perferendis dignissimos cumque blanditiis eaque quisquam. Voluptas aperiam culpa deserunt nesciunt quos consectetur eum voluptates veritatis placeat odit architecto blanditiis reiciendis aliquid minus dolore hic nihil dolores mollitia neque, at repellat. Iure suscipit quisquam repudiandae commodi perferendis? Maiores dignissimos itaque possimus velit alias illo ut? Ad repellendus quasi minus nemo incidunt repudiandae? Enim odit, et facere excepturi illum qui!                            </p>
                         </div>
                     </div>
                 </div>
--- resources/js/Pages/NewsPage.jsx
+++ resources/js/Pages/NewsPage.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Footer from "@/Components/Footer";
 import Header from "@/Components/Navbar";
 import NewsCard from "@/Components/NewsCard";
@@ -9,10 +10,37 @@ const NewsPage = () => {
             <Header />
             <div className="pt-[120px]">
                 <div className="w-11/12 lg:w-5/6 mx-auto container mb-32">
+                    <div
+                        className="relative h-48 md:h-64 xl:h-72 w-full flex items-end justify-start text-left bg-cover bg-center mb-5 rounded-lg"
+                        style={{
+                            backgroundImage:
+                                "url('build/assets/images/hero_image.png')",
+                        }}
+                    >
+                        <div className="absolute top-0 right-0 bottom-0 left-0 bg-gradient-to-b from-transparent to-gray-900 rounded-lg"></div>
+                        <div className="absolute top-0 right-0 left-0 mx-5 mt-2 flex justify-between items-center">
+                            <div className="text-white font-regular flex flex-col justify-start">
+                                <span className="text-2xl leading-0 font-semibold">
+                                    20
+                                </span>
+                                <span className="-mt-2">Jun</span>
+                            </div>
+                        </div>
+                        <main className="px-3 pb-5 z-10">
+                            <a
+                                href=""
+                                className="text-lg lg:text-2xl tracking-tight font-medium leading-6 font-regular text-white hover:underline"
+                            >
+                                <p className="line-clamp-2 text-ellipsis overflow-hidden ...">
+                                   Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed voluptate ex magnam eaque quo facere id dolore error nemo assumenda!
+                                </p>
+                            </a>
+                        </main>
+                    </div>
                     <div className="flex gap-3 mb-4">
-                        <NewsCard classname='md:text-lg'/>
-                        <NewsCard classname='md:text-lg'/>
-                        <NewsCard classname='md:text-lg'/>
+                        <NewsCard classname="md:text-lg" />
+                        <NewsCard classname="md:text-lg" />
+                        <NewsCard classname="md:text-lg" />
                     </div>
 
                     <h2 className="text-xl 2xl:text-4xl font-bold mb-2">
@@ -34,10 +62,10 @@ const NewsPage = () => {
                     <div className="h-[1px] lg:h-0.25 w-full bg-gray-500 mb-10"></div>
 
                     <div className="gap-10 md:flex">
-                        <NewsCard classname="text-xl"/>
-                        <NewsCard classname="text-xl"/>
-                        <NewsCard classname="text-xl"/>
-                        <NewsCard classname="text-xl"/>
+                        <NewsCard classname="text-xl" />
+                        <NewsCard classname="text-xl" />
+                        <NewsCard classname="text-xl" />
+                        <NewsCard classname="text-xl" />
                     </div>
                 </div>
             </div>
