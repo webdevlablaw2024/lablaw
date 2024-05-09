3a50c1458660823aee1f4f2e6128ec5d5808ffbc
Wahyu <wahyuardiansyah764@gmail.com>
2024-05-09 09:23:28 +0700

    Revert "add about, team, and from page"
    This reverts commit 115004a4a9587df840e8df30b734664fb6f0d32e.

--- resources/js/Components/FounderCard.jsx
+++
@@ -1,9 +0,0 @@
-export default function FounderCard({ classname="", children, ...props }){
-  return (
-    <>
-    <div className="w-[478px] h-[683px] bg-gray-400">
-
-    </div>
-    </>
-  );
-}
--- resources/js/Components/SubtitleText.jsx
+++
@@ -1,7 +0,0 @@
-export default function SubtitleText({ classname='', children }){
-  return (
-    <>
-      <h2 className={`text-2xl xl:text-3xl font-bold mb-2 ` + classname}>{children}</h2>
-    </>
-  );
-}
