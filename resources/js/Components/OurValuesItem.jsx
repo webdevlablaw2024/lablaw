f4c06022b25ea6582c7cf48e7b0e4f49b8f88e5c
Wahyu <wahyuardiansyah764@gmail.com>
2024-05-23 22:09:25 +0700

    update

--- resources/js/Components/OurValuesItem.jsx
+++ resources/js/Components/OurValuesItem.jsx
@@ -1,6 +1,6 @@
 export default function OurValuesItem({ classname = "", icon, desciption }) {
   return (
-    <div className="flex gap-2 text-left">
+    <div className="flex gap-2 text-left mb-5">
       <img src={icon} className="w-9" alt="Icon" />
       <p className="text-md xl:text-lg">{desciption}</p>
     </div>
