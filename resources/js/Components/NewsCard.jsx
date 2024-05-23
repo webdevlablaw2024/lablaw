a3d5823d39d02e140508ef5df5c6e815e4cd8d15
Wahyu <wahyuardiansyah764@gmail.com>
2024-05-24 00:57:20 +0700

    update show article

--- resources/js/Components/NewsCard.jsx
+++ resources/js/Components/NewsCard.jsx
@@ -6,6 +6,7 @@ export default function NewsCard({ classname = "", ...props }) {
             <div className={`` + classname}>
                 <img
                     src={`${window.location.origin}/storage/artikel/images/${props?.article?.image}`}
+                    // src="./images/hero_image.png"
                     alt="News"
                     className="object-cover rounded-md w-full h-2/3"
                 />
